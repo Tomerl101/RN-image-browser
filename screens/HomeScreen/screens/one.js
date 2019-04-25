2419eb8b75089434801d2e7f9fff02e6aab0f1bd
Tomer <tomerl101@gmail.com>
2019-04-25 17:48:41 +0300

    start expo app

---
+++ screens/HomeScreen/screens/one.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default class oneScreen extends React.Component {
+  // static navigationOptions = {
+  //   header: null
+  // }
+
+  render() {
+    return (
+      <View>
+        <Text>this is screen 1</Text>
+      </View>
+    )
+  }
+}
---
+++ screens/LinksScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class LinksScreen extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Links'
+  // }
+
+  render() {
+    return (
+      <View>
+        <Text>Change this text and your app will automatically reload.</Text>
+      </View>
+    )
+  }
+}
